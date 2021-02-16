329d36aaec670f30d759d06cd86f3aadce4881a7
changwng@gmail.com <changwng@gmail.com>
2021-02-16 18:28:48 +0900

    다시 추가 하는 부분

--- README.md
+++ README.md
@@ -9,9 +9,111 @@ Spring Boot 2.1 기반으로 Spring Security OAuth2를 살펴보는 프로젝트
 * Java8
 
 # 목차
+https://velog.io/@rohkorea86/oAuth-oAuth%EB%A5%BC-%EC%9D%B4%ED%95%B4%ED%95%98%EB%8A%94-%EC%9A%A9%EC%96%B4%EC%A0%95%EB%A6%AC
+oAuth의 이해 관계자들
+Resource Server(리소스 서버): 제어할 수 있는 자원을 가진 서버
+     kakao, facebook, naver 회사를 의미합니다
+Resource Owner(리소스 오너): 자원의 소유자( 브라우저, 로그인 자원(Resource)를 가지고 있는 사람(Owner)으로 해당 서비스를 이용하려는 유저(User)입니다.)
+client(클라이언트): 리소스 서버에 접속해서 정보를 가져가는 대상 : 우리의 현재 만들고 있는 서비스(service) 혹은 어플리케이션(application)을
 
 * [step-01 OAuth2 인증 방식 Flow 및 Sample Code](https://github.com/cheese10yun/springboot-oauth2/blob/master/docs/OAuth2-Grant.md)
 * [step-02 토큰과 클라이언트 정보 RDBMS 저장](https://github.com/cheese10yun/springboot-oauth2/blob/master/docs/OAuth2-RDBMSt.md)
 * [step-03 Redis를 이용한 토큰 저장 작업중...]()
 
 **step-XX Branch 정보를 의미합니다. 보고 싶은 목차의 Branch로 checkout을 해주세요**
+
+
+http://www.bubblecode.net/en/2016/01/22/understanding-oauth2/
+oles
+OAuth2 defines 4 roles :
+
+Resource Owner: generally yourself.
+Resource Server: server hosting protected data (for example Google hosting your profile and personal information).
+Client: application requesting access to a resource server (it can be your PHP website, a Javascript application or a mobile application).
+Authorization Server: 서버가 클라이언트에 액세스 토큰을 발행합니다. 이 토큰은 클라이언트가 리소스 서버를 요청하는 데 사용됩니다. 이 서버는 권한 부여 서버 (동일한 물리적 서버 및 동일한 응용 프로그램)와 동일 할 수 있으며 종종 그럴 수 있습니다.
+Tokens
+토큰은 권한 부여 서버에 의해 생성 된 임의의 문자열이며 클라이언트가 요청할 때 발행됩니다.
+There are 2 types of token:
+
+Access Token: 이는 타사 응용 프로그램에서 사용자 데이터에 액세스하는 것을 허용하기 때문에 가장 중요합니다. 이 토큰은 클라이언트에 의해 매개 변수 또는 요청의 헤더로 리소스 서버에 전송됩니다. 권한 부여 서버에 의해 정의 된 제한된 수명이 있습니다. 가능한 한 빨리 기밀로 유지해야하지만, 특히 클라이언트가 Javascript를 통해 리소스 서버에 요청을 보내는 웹 브라우저 인 경우 항상 가능한 것은 아닙니다.
+Refresh Token: 이 토큰은 액세스 토큰과 함께 발급되지만 후자와 달리 클라이언트에서 리소스 서버로의 각 요청에서 전송되지 않습니다. 만료되었을 때 액세스 토큰을 갱신하기 위해 권한 부여 서버로 전송되는 역할을합니다. 보안상의 이유로 항상이 토큰을 얻을 수있는 것은 아닙니다. 나중에 어떤 상황에서.
+
+
+curl -X POST   http://localhost:8080/oauth/token   -H 'Content-Type: application/x-www-form-urlencoded'   -d 'username=user&password=pass&grant_type=password&scope=read_profile'
+
+
+ 테스트 방법
+3) Resource Owner Password Credentials Grant 방식
+curl -X POST http://localhost:8080/oauth/token -H "Authorization: Basic Y2xpZW50OnBhc3N3b3Jk"  -H "Content-Type: application/x-www-form-urlencoded" -d "username=user&password=pass&grant_type=password&scope=read_profile"
+ 3.1) 리턴값으로 access token및 refresh token이 리턴된다.
+ {"access_token":"3d52838f-8640-42e1-97eb-1b2e4f9002fd","token_type":"bearer","re
+fresh_token":"7d38a264-b99c-495a-bb9e-c9e6bc30ad30","expires_in":2999,"scope":"r
+ead_profile"}
+
+curl -X POST \
+'http://localhost:8080/oauth/check_token' \
+-d 'token=a1849c64-2ca6-4d32-8f15-6cf01029dfb6'
+
+{"access_token":"3d52838f-8640-42e1-97eb-1b2e4f9002fd","token_type":"bearer","refresh_token":"7d38a264-b99c-495a-bb9e-c9e6bc30ad30","expires_in":2790,"scope":"read_profile"}
+
+  1) Authorization Code Grant Type
+  1.1)  http://localhost:8080/oauth/authorize?client_id=client&redirect_uri=http://localhost:9000/callback&response_type=code&scope=read_profile 
+ 1.2) Approval, Deny로 표시됨
+ 1.3) 리턴값 code 리턴
+  http://localhost:9000/callback?code=dbdQJN
+Xy3BOA
+curl -X POST http://localhost:8080/oauth/token -H "Authorization: Basic Y2xpZW50OnBhc3N3b3Jk"  -H "Content-Type: application/x-www-form-urlencoded"   -d "code=dbdQJN&grant_type=authorization_code&redirect_uri=http%3A%2F%2Flocalhost%3A9000%2Fcallback&scope=read_profile"
+{"access_token":"3d52838f-8640-42e1-97eb-1b2e4f9002fd","token_type":"bearer","re
+fresh_token":"7d38a264-b99c-495a-bb9e-c9e6bc30ad30","expires_in":1874,"scope":"r
+ead_profile"}
+
+//GET /oauth/authorize 요청은 클라이언트가 리소스 서버의 API를 사용하기 위해 사용자(리소스 소유자)에게 권한 위임 동의를 받기 위한 페이지를 출력하는 기능을 수행한다
+https://jsonobject.tistory.com/372
+https://jsonobject.tistory.com/374
+https://jsonobject.tistory.com/373
+### grant_type = 'authorization_code'
+curl -X POST \
+'http://localhost:8080/oauth/token' \
+-U 'some_client_id:some_client_secret' \
+-d 'grant_type=authorization_code' \
+-d 'code=DO8jTT' \
+-d 'redirect_uri=http://localhost'
+
+### grant_type = 'client_credentials'
+curl -X POST \ 'http://localhost:8080/oauth/token' \ 
+-U 'some_client_id:some_client_secret' \ 
+-d 'grant_type=client_credentials'
+
+### grant_type = 'refresh_token'
+curl -X POST \ 'http://localhost:8080/oauth/token' \
+-U 'some_client_id:some_client_secret' \
+-d 'grant_type=refresh_token' \
+-d 'refresh_token=9c2d4115-ebd2-4ff5-8f69-1c2e1198cf52'
+
+
+# 성공 응답
+{
+    "access_token": "a1849c64-2ca6-4d32-8f15-6cf01029dfb6",
+    "token_type": "bearer",
+    "refresh_token": "9c2d4115-ebd2-4ff5-8f69-1c2e1198cf52",
+    "expires_in": 43199,
+    "scope": "read:users"
+}
+
+# code 파라메터가 유효하지 않을 경우
+{
+    "error": "invalid_grant",
+    "error_description": "Invalid authorization code: DO8jTT"
+}
+
+# redirect_uri 파라메터가 유효하지 않을 경우
+{
+    "error": "invalid_grant",
+    "error_description": "Redirect URI mismatch."
+}
+
+# authorization_code 파라메터가 유효하지 않을 경우
+{
+    "error": "unsupported_grant_type",
+    "error_description": "Unsupported grant type: authorization_code"
+}
--- src/main/java/com/example/oauth2/config/AuthorizationServerConfig.java
+++ src/main/java/com/example/oauth2/config/AuthorizationServerConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.provider.approval.Approval;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
@@ -24,42 +25,136 @@ import javax.sql.DataSource;
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final AuthenticationManager authenticationManager;
-    private final DataSource dataSource;
+    private final DataSource dataSource; // (1) 토큰에 대한 영속화를 진행하기 위해서 Datasoruce 의존성을 주입받습니다.
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients
-                .jdbc(dataSource)
+        clients.jdbc(dataSource) // (5) clinet inMemory() 방식에서 jdbc() 방식으로 변경합니다. 의존성은 dataSource 주입해줍니다.
         ;
-//                .inMemory()
-//                .withClient("client")
-//                .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password
-//                .redirectUris("http://localhost:9000/callback")
-//                .authorizedGrantTypes("authorization_code", "implicit", "password", "client_credentials", "refresh_token")
+//                .inMemory() //(1): 간단한 설정을 위해 메모리에 저장시키겠습니다.
+//                .withClient("client") //(2): 클라이언트 이름을 작성합니다.
+//                .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password//(3): 시크릿을 작성해야합니다. 스프링 시큐리티 5.0 부터는 암호화 방식이 조 변경되서 반드시 암호화해서 저장하는 것을 권장합니다. 해당 암호는 password입니다. (현재 프로젝트는 스프링부트 2.1 이기 때문에 스프링 시큐리티 5.1 의존성을 주입받습니다.)
+//                .redirectUris("http://localhost:9000/callback") //(4): 리다이렉트 URI을 설정합니다.
+//                .authorizedGrantTypes("authorization_code", "implicit", "password", "client_credentials", "refresh_token")//
 //                .accessTokenValiditySeconds(120)
 //                .refreshTokenValiditySeconds(240)
 //                .scopes("read_profile");
     }
 
 
+    /* 1) Authorization Code Grant Type 방식
+      @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory() // (1)
+                .withClient("client") //(2)
+                .secret("{bcrypt} $2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  //(3) password
+                .redirectUris("http://localhost:9000/callback") // (4)
+                .authorizedGrantTypes("authorization_code") // (5)(5): Authorization Code Grant 타입을 설정합니다.
+                .scopes("read_profile"); // (6)
+    }
+    */
+    /* 2) Implicit Grant 방식
+        @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client")
+                .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password
+                .redirectUris("http://localhost:9000/callback")
+                .authorizedGrantTypes("authorization_code", "implicit") // (1) implicit 추가
+                .accessTokenValiditySeconds(120)
+                .refreshTokenValiditySeconds(240)
+                .scopes("read_profile");
+    }
+    응답 타입이 token 일 경우 암시적 승인 타입에 해당합니다
+    */
+    /* 3) Resource Owner Password Credentials Grant 방식
+    public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client")
+                .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password
+                .redirectUris("http://localhost:9000/callback")
+                .authorizedGrantTypes("authorization_code", "implicit", "password") // (1) password 타입 추가
+                .accessTokenValiditySeconds(120) //  access token 만료시간
+                .refreshTokenValiditySeconds(240) // refresh token 만료시간
+                .scopes("read_profile");
+    }
+
+
+    //(2)
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+                //@formatter:off
+                endpoints
+                        .authenticationManager(authenticationManager) // Bean 등록은 SecurityConfig 에서 등록합니다.
+                ;
+                //@formatter:on
+            }
+        }
+    
+      4) Client Credentials Grant Type 방식
+         @Override
+public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients
+            .inMemory()
+            .withClient("client")
+            .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password
+            .redirectUris("http://localhost:9000/callback")
+            .authorizedGrantTypes("authorization_code", "implicit", "password", "client_credentials") // client_credentials 추가
+            .accessTokenValiditySeconds(120)
+            .refreshTokenValiditySeconds(240)
+            .scopes("read_profile");
+}
+    // 코드의 변경사항은 client_credentials 변경 뿐입니다.
+    리소스 주인이 어떤 권한인증을 하지 않기 때문에 Refresh Token을 넘겨주지 않는것이 바람직합니다.
+    5) Refresh Token Grant Type 방식
+
+      @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("client")
+                .secret("{bcrypt}$2a$10$iP9ejueOGXO29.Yio7rqeuW9.yOC4YaV8fJp3eIWbP45eZSHFEwMG")  // password
+                .redirectUris("http://localhost:9000/callback")
+                .authorizedGrantTypes("authorization_code", "implicit", "password", "client_credentials", "refresh_token") // refresh_token 추가
+                .accessTokenValiditySeconds(120)
+                .refreshTokenValiditySeconds(240)
+                .scopes("read_profile");
+    }
+        */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
         //@formatter:off
-        endpoints
+        endpoints  //(4) 2,3 번에서 생성한 객체을 AuthorizationServerEndpointsConfigurer 객체에 넣어줍니다.
                 .approvalStore(approvalStore())
                 .tokenStore(tokenStore())
                 .authenticationManager(authenticationManager)
         ;
         //@formatter:on
+        /* endpoints
+            .accessTokenConverter(jwtAccessTokenConverter())
+            .userDetailsService(userDetailsService);
+            */
     }
+    @Override
+        public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception
+        {
+        oauthServer.checkTokenAccess("isAuthenticated()");    
+        }
 
     @Bean
-    public TokenStore tokenStore() {
+    public TokenStore tokenStore() { //(2) 주입 받은 Datasoruce 의존성을 기반으로 JdbcTokenStore을 생성합니다.
         return new JdbcTokenStore(dataSource);
     }
 
     @Bean
-    public ApprovalStore approvalStore() {
+    public ApprovalStore approvalStore() { //(3) 2번과 마찬가지로 Datasoruce을 주입시켜 JdbcApprovalStore을 생성합니다
         return new JdbcApprovalStore(dataSource);
     }
 
